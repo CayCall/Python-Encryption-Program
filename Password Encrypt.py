3d38080ac99f02676c38ca89d7fd822292e0b114
Caleb Cameron Calvin <calvincaleb09@gmail.com>
2025-09-21 18:15:29 +0200

    changes

--- Password Encrypt.py
+++ Password Encrypt.py
@@ -1,8 +1,14 @@
 import random
 import string
+from time import sleep
 
 username = input("What is your username ?\n")
+print("Your username has been saved!")
 password = input("What is your password ?\n")
+print("Your password is now being saved and encrypted!")
+sleep(1)
+print("Encrypting...")
+sleep(2)
 
 charset = string.punctuation + string.digits + string.ascii_letters
 charset= list(charset)
@@ -12,14 +18,12 @@ shuffled_charset = charset.copy()
 random.shuffle(shuffled_charset)
 
 encryption_dict = dict(zip(charset, shuffled_charset))
-print(encryption_dict)
 encyptionValue = ""
 
 #Encypt
 for char in password:
     if char in encryption_dict:
         encyptionValue += encryption_dict[char]
-        print(encyptionValue)
     else:
         encyptionValue += char
 
